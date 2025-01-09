bed1075627c0b1335b11f482fc2dba9b80f184e4
BoHofland <bo.hofland@student.fontys.nl>
2025-01-09 13:01:15 +0100

    begin van animatie maken

---
+++ slide-in-animation/app/components/AnimatedComponent.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const AnimatedComponent = () => {
+  return (
+    <div className="flex justify-center items-center h-screen bg-gray-900">
+      <div className="animate-bounce bg-blue-500 text-white font-bold py-2 px-4 rounded">
+        Hallo Wereld!
+      </div>
+    </div>
+  );
+};
+
+export default AnimatedComponent;
